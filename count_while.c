ff022227a75655fa59e7c116b4e7028b17cf1d84
Your GitHub Username <aswinalchristy@gmail.com>
2026-09-15 14:56:00 +0530

    completed sum of digits using while

--- count_while.c
+++ count_while.c
@@ -12,12 +12,14 @@ int main(){
     printf("Enter the number:");
     scanf("%d",&n);
 
-    
+    if(n==0){
+        count=1;
+    }else{
         while(n>0){
         n=n/10;
         count++;
         }
-    
+    }
  
   
    printf("The count for the number you entered is: %d",count);
