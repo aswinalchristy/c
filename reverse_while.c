7dd97d36b490bcbd446e777dc934db2a087ba609
Your GitHub Username <aswinalchristy@gmail.com>
2026-09-15 19:23:15 +0530

    completed reverse of a number using while loop

--- reverse_while.c
+++ reverse_while.c
@@ -1,5 +1,5 @@
 /*Here is Problem 28 for your practice!Problem 28: Reverse a NumberProblem Statement:Write a C program using a while loop that asks the user for a positive integer $N$, reverses its digits, and prints the reversed number.(Example: If the user enters 425, the output should be 524. If the user enters 1200, the output should be 21.)*/
-
+//reverse
 #include <stdio.h>
 
 int main() {
