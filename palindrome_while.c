aa381000a0e4c3e05d704835d7b7cb6a5baa5931
Your GitHub Username <aswinalchristy@gmail.com>
2026-09-16 10:50:28 +0530

    completed armstrong problem using while loop

--- palindrome_while.c
+++ palindrome_while.c
@@ -1,5 +1,5 @@
 /*Here is Problem 29 for your practice!Problem 29: Check if a Number is a PalindromeProblem Statement:Write a C program using a while loop that asks the user for a positive integer $N$ and checks whether it is a Palindrome. A palindrome is a number that reads the same backward as forward (e.g., 121, 1331, 4554).(Example: If the user enters 121, print 121 is a Palindrome. If the user enters 123, print 123 is not a Palindrome.)*/
-
+//palindrome checker program
 #include<stdio.h>
 int main(){
     int n,orig_n;
