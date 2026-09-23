f755d86f73dce9eca9a77eca885ba5cc91531ee9
Your GitHub Username <aswinalchristy@gmail.com>
2026-09-23 12:59:22 +0530

    array declaring using for loop

---
+++ learningC/array2.c
@@ -0,0 +1,21 @@
+/*Now, let's write it using a for loop: Once you see how individual indices work (arr[0], arr[1], arr[2]), we replace the repeated lines with a simple for loop using i*/
+
+#include<stdio.h>
+int main(){
+    int arr[3];
+
+    for (int i=0;i<3;i++){
+        printf("enter the number for index [%d]",i);
+
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Your array contains\n");
+    for(int i=0;i<3;i++){
+        printf("the element at index [%d] is %d\n",i,arr[i]);
+    }
+    return 0;
+}
+
+
+    
