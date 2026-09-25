49e9bf32a1855e5ff09d730a1611a85fd72c639c
Your GitHub Username <aswinalchristy@gmail.com>
2026-09-25 09:47:39 +0530

    done

--- reverse_array.c
+++ reverse_array.c
@@ -1,25 +1,15 @@
 //reversing an array
-
-
 #include<stdio.h>
-
 int main(){
     int arr[100];
     int n;
-
     printf("enter the size of the array");
     scanf("%d",&n);
-
     printf("enter the array elements");
-
     for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-        
+        scanf("%d",&arr[i]);   
     }
     for(int i=n-1;i>=0;i--){
         printf("%d ",arr[i]);
     }
-
-
 }
-    
